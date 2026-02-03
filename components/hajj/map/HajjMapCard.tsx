e46af3938a6ed089943b34c2f7ec6e8803e8af37
Umer Hafeez Mir <umerhafeez.mir@outlook.com>
2026-02-03 17:42:16 +0530

    Update HajjMapCard.tsx

--- components/hajj/map/HajjMapCard.tsx
+++ components/hajj/map/HajjMapCard.tsx
@@ -1,1 +1,21 @@
+// components/hajj/map/HajjMapCard.tsx
+"use client";
+
+import Card from "@/components/Card";
+import MapPlaceholder from "@/components/hajj/map/MapPlaceholder";
+import ManualLocationPicker from "@/components/hajj/map/ManualLocationPicker";
+import MapLegend from "@/components/hajj/map/MapLegend";
+
+export default function HajjMapCard() {
+  return (
+    <Card title="MAP">
+      <div className="space-y-3">
+        {/* 45% height target later when real map; placeholder is compact */}
+        <MapPlaceholder />
+        <MapLegend />
+        <ManualLocationPicker />
+      </div>
+    </Card>
+  );
+}
 
